d16161df989b823f602429c4cdd065efe523f711
Lyza Danger Gardner <lyza@lyza.com>
2011-09-04 18:10:58 -0700

    Added reference text file with user agents from chapter
    This is a lot less painful than typing them in. Also added the copying
    of the file into the "student" variant via the build script.

--- build.sh
+++ build.sh
@@ -8,4 +8,5 @@ mkdir -p device_classes
 cp -LR ../03_device_class_test/01_begin device_classes/test_classes
 cp -LR ../04_device_class_content/01_begin device_classes/adapt_content
 cp ../04_device_class_content/02_end/index.php device_classes/adapt_content/index_solution.php
+cp ../shared/useful_user_agents.txt .
 find . -name config.php | xargs rm
