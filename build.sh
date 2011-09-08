820c0d22de48bdcada649ca3af2584763ad048e7
Lyza Danger Gardner <lyza@lyza.com>
2011-09-08 16:11:43 -0700

    O! Build Script, you also needed updatin' to account for directory structure change-a-roos

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ cp -LR ../02_panic_button/01_begin panic_button
 mkdir -p device_classes
 cp -LR ../03_device_class_test/01_begin device_classes/test_classes
 cp -LR ../04_device_class_content/01_begin device_classes/adapt_content
-cp ../04_device_class_content/02_end/index.php device_classes/adapt_content/index_solution.php
+cp ../04_device_class_content/03_end/index.php device_classes/adapt_content/index_solution.php
 cp ../shared/useful_user_agents.txt .
 find . -name config.php | xargs rm
