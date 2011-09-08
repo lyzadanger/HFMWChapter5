67eb4d1fbf3daab694fee29b80b4fb841a47626d
Lyza Danger Gardner <lyza@lyza.com>
2011-09-08 16:09:50 -0700

    Mild-to-moderate amount of minor futzing.

--- 03_device_class_test/01_begin/device.php
+++ 03_device_class_test/01_begin/device.php
@@ -1,6 +1,1 @@
-<?php
-require_once('config.php');
-$wurflConfig = new WURFL_Configuration_XmlConfig($wurflConfigFile);
-$wurflManagerFactory = new WURFL_WURFLManagerFactory($wurflConfig);
-$wurflManager = $wurflManagerFactory->create();
-?>
+../shared/device.php
